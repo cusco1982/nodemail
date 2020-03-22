8ae3cefebb097def1b77b5464a92405de1141a42
Julian <cusco2891@gmail.com>
2020-03-22 04:05:53 -0400

    -routes//

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 require('dotenv').config();
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(routes);
+// app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WJBest", {useNewUrlParser: true}).then(() => {
   console.log("Mongo connected")
