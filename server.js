fb15a51183e31cc0de65070350d1bb79e5e9f2d3
Julian <cusco2891@gmail.com>
2020-03-22 03:53:23 -0400

    mongodb connected

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+require('dotenv').config();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,7 +15,10 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WJBest", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WJBest", {useNewUrlParser: true}).then(() => {
+  console.log("Mongo connected")
+  console.log(process.env.MONGODB_URI)
+}).catch(err => console.log(err));
 
 // Start the API server
 app.listen(PORT, function() {
